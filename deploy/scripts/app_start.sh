ff07b54c01cfadaec3560f23a8c364d01afa09dc
Dong Mai <dong@gmail.com>
2023-08-19 18:15:10 +0700

    update buildspec

--- deploy/scripts/app_start.sh
+++ deploy/scripts/app_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 pwd
-cd ./deploy
 docker-compose up -d
--- deploy/scripts/before_install.sh
+++ deploy/scripts/before_install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Important need to login aws ecr to be able to pull image
 pwd
-cd ./deploy
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 295899798296.dkr.ecr.us-east-1.amazonaws.com
 docker-compose pull
