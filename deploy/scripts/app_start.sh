1cfae9ec0d1cdc1b9c0cc29d1eb65a4f31c5a453
Dong Mai <dong@gmail.com>
2023-08-19 18:21:39 +0700

    update buildspec

--- deploy/scripts/app_start.sh
+++ deploy/scripts/app_start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 pwd
+cd /home/ubuntu/simplenode2/deploy
 docker-compose up -d
--- deploy/scripts/before_install.sh
+++ deploy/scripts/before_install.sh
@@ -2,4 +2,6 @@
 # Important need to login aws ecr to be able to pull image
 pwd
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 295899798296.dkr.ecr.us-east-1.amazonaws.com
+
+cd /home/ubuntu/simplenode2/deploy
 docker-compose pull
